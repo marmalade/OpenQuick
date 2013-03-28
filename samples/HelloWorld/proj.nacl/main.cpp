bfc16b2188e6ee5a73f71e8e1ea2ae36ffe3e31e
Sam Clegg <sbc@google.com>
2013-03-28 10:25:53 -0700

    NaCl port

---
+++ samples/HelloWorld/proj.nacl/main.cpp
@@ -0,0 +1,31 @@
+#include "../AppDelegate.h"
+#include "cocos2d.h"
+#include "CCInstance.h"
+#include "CCModule.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string>
+
+USING_NS_CC;
+
+AppDelegate g_app;
+
+void* cocos_main(void* arg)
+{
+    fprintf(stderr, "in cocos_main\n");
+    int rtn = CCApplication::sharedApplication()->run();
+    fprintf(stderr, "app run returned: %d\n", rtn);
+    return NULL;
+}
+
+namespace pp
+{
+
+Module* CreateModule()
+{
+  return new CocosPepperModule();
+}
+
+}
